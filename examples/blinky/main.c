2c1953fd5edd02430af500c46a7c4b30a0da4ef2
Hermano Cabral <hcabral00@gmail.com>
2021-06-17 18:37:55 -0300

    Full working version using reactor

--- examples/blinky/main.c
+++ examples/blinky/main.c
@@ -4,23 +4,21 @@
 #include "hal.h"
 
 
-int main(void) {
-    int i;
-	
-    /* vt_add_non_rt_handler(read_pin_cb, 10, 1); */
-    /* reactor_start(); */
-
-    while(1) {
-        for (i=0; i<2000000; i++)
-            asm("NOP");
-
-        if (gpio_read_pin_latch(GPIOB, 27) != 0) {
-            /* Set clear register */
-            gpio_clear_pin(GPIOB, 27);
-        } else {
-            /* Set pin */
-            gpio_set_pin(GPIOB, 27);
-        }
+uint32_t blinky_cb() {
+    if (gpio_read_pin_latch(GPIOB, 27) != 0) {
+        /* Set clear register */
+        gpio_clear_pin(GPIOB, 27);
+    } else {
+        /* Set pin */
+        gpio_set_pin(GPIOB, 27);
     }
+
+    return 0;
+}
+
+int main(void) {
+    vt_add_non_rt_handler(blinky_cb, 500, 1);
+    reactor_start();
+
     return 0;
 }
